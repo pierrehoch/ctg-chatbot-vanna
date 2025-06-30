d5333dcf130fcfea962eeff99c77ea0a061b7f95
Pierre Hochedez <pierre.hochedez@outlook.fr>
2025-06-30 19:11:42 +0200

    Handle pysqlite3 import and module replacement for sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import time
 import streamlit as st
 from vanna_calls import (
